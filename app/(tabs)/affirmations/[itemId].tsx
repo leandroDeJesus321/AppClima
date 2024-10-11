177c74c712495b0bad79d32a449c2802875dcabe
leandrodejesus <lean_silva100@hotmail.com>
2024-10-11 11:31:12 -0300

    4º commit, ajustado código, nomes de arquivos e reescrito partes do código para funcionar normalmente.

--- app/(tabs)/affirmations/[itemId].tsx
+++ app/(tabs)/affirmations/[itemId].tsx
@@ -1,13 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, ImageBackground, Pressable, ScrollView } from "react-native";
 import React, { useEffect, useState } from "react";
-import { Stack, useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams, router } from "expo-router";
 import { GalleryPreviewData } from "@/constants/models/AffirmationsCategory";
 import AFFIRMATION_GALLERY from "@/constants/affirmation-gallery";
+import AppGradient from "@/components/AppGradient";
+import { AntDesign } from "@expo/vector-icons";
 
 const AffirmationsPractice = () => {
   const {itemId} = useLocalSearchParams();
 
   const [affirmation, setAffirmation] = useState<GalleryPreviewData>();
+  const [sentences, setSentences] = useState<string[]>([]);
 
   useEffect(()=>{
     for(let idx=0; idx < AFFIRMATION_GALLERY.length; idx++){
@@ -19,7 +22,16 @@ const AffirmationsPractice = () => {
       );
 
       if(affirmationToStart){
+
         setAffirmation(affirmationToStart);
+
+        const affirmationsArray = affirmationToStart.text.split(".");
+
+        if(affirmationsArray[affirmationsArray.length - 1] === ''){
+          affirmationsArray.pop();
+        }
+
+        setSentences(affirmationsArray);
 
         return
 
@@ -31,8 +43,39 @@ const AffirmationsPractice = () => {
 
 
   return (
-    <View>
-      <Text>AffirmationsPractice</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={affirmation?.image}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <AppGradient colors={["rgba(0,0,0,0.3)","rgba(0,0,0,0.9)"]}>
+            <Pressable
+                onPress={()=>router.back()}
+                className="absolute top-16 left-6 z-10"
+            >
+              <AntDesign name="leftcircleo" size={50} color="white" />
+            </Pressable>
+            <ScrollView
+              className="mt-20"
+              showsVerticalScrollIndicator={false}
+            >
+              <View
+                className="h-full justify-center"
+              >
+                <View className="h-4/5 justify-center" >
+                  {sentences?.map((sentence,idx)=>(
+                    <Text key={idx} className="text-white text-3xl mb-12 font-bold text-center">
+                      {sentence}.
+                    </Text>
+                  ))}
+                  
+                </View>
+              </View>
+
+            </ScrollView>
+        </AppGradient>
+      </ImageBackground>
     </View>
   );
 };
