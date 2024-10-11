3d8db5771bf12e6c967d756d1a33585eecc6719b
leandrodejesus <lean_silva100@hotmail.com>
2024-10-11 09:05:24 -0300

    3º commit, criado a tela de detalhes de imagens de meditação.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,48 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Tabs } from 'expo-router'
-import Colors from '@/constants/Colors'
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
-import Entypo from '@expo/vector-icons/Entypo';
+import { View, Text } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import Colors from "@/constants/Colors";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import Entypo from "@expo/vector-icons/Entypo";
 
 const _layout = () => {
   return (
     <Tabs
-    screenOptions={{
-        headerShown: false, 
-        tabBarActiveTintColor: Colors.primary,}}
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: Colors.primary,
+      }}
     >
-        <Tabs.Screen 
-            name="nature_meditate" 
-            options={{
-                tabBarLabel: "Meditate",
-                tabBarIcon: ({color}) => (
-                    <MaterialCommunityIcons 
-                        name="flower-tulip" 
-                        size={24} 
-                        color={color}
-                    />
-                )
-            }} 
-        />
-        <Tabs.Screen 
-            name="affirmations" 
-            options={{
-                tabBarLabel: "Affirmations",
-                tabBarIcon: ({color}) => (
-                    <Entypo 
-                        name="open-book" 
-                        size={24} 
-                        color={color}
-                    />
-                )
-            }} 
-        />
+      <Tabs.Screen
+        name="nature_meditate"
+        options={{
+          tabBarLabel: "Meditate",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="flower-tulip"
+              size={24}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="affirmations"
+        options={{
+          tabBarLabel: "Affirmations",
+          tabBarIcon: ({ color }) => (
+            <Entypo name="open-book" size={24} color={color} />
+          ),
+        }}
+      />
     </Tabs>
-  )
-}
+  );
+};
 
-
-
-export default _layout
-
+export default _layout;
