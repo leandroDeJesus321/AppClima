52fa8896d0bedebcccf081336ed4703b25bbb507
leandrodejesus <lean_silva100@hotmail.com>
2024-10-14 09:15:23 -0300

    5º commit, adicionado cronometro de meditação, ainda não ajustado para incrementar valor númerico pelo próprio usuário.

--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -1,5 +1,5 @@
 import { View, Text, ImageBackground, Pressable } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import MEDITATION_IMAGES from '@/constants/meditation-images'
 import AppGradient from '@/components/AppGradient'
@@ -10,38 +10,66 @@ import CustomButton from '@/components/custombutton'
 const Meditate = () => {
 
     const {id} = useLocalSearchParams();
+    const [secondsRemaining, setSecondsRemaining] = useState(10);
+    const [isMeditating, setMeditating] = useState(false);
 
-  return (
-    <View className='flex-1'>
-        <ImageBackground
-            source={MEDITATION_IMAGES[Number(id) - 1]}
-            resizeMode='cover'
-            className='flex-1'
-        >
-            <AppGradient
-                colors={["transparent","rgba(0,0,0,0.8)"]}
+    useEffect(()=>{
+        let timerId: NodeJS.Timeout;
+        //
+        if(secondsRemaining === 0){
+            setMeditating(false);
+            return;
+        }
+
+        if(isMeditating){
+            timerId = setTimeout(() => {
+                setSecondsRemaining(secondsRemaining - 1)
+            }, 1000);
+            
+        }
+
+        return () =>{
+            clearTimeout(timerId);
+        }
+
+    },[secondsRemaining, isMeditating])
+
+    const formattedTimeMinutes = String(Math.floor(secondsRemaining / 60)).padStart(2,"0")
+    const formattedTimeSeconds = String(Math.floor(secondsRemaining % 60)).padStart(2,"0")
+
+    return (
+        <View className='flex-1'>
+            <ImageBackground
+                source={MEDITATION_IMAGES[Number(id) - 1]}
+                resizeMode='cover'
+                className='flex-1'
             >
-                <Pressable onPress={()=>router.back()} className='absolute top-16 left-6 z-10' >
-                    <AntDesign name="leftcircleo" size={50} color="white" />
-                </Pressable>
+                <AppGradient
+                    colors={["transparent","rgba(0,0,0,0.8)"]}
+                >
+                    <Pressable onPress={()=>router.back()} className='absolute top-16 left-6 z-10' >
+                        <AntDesign name="leftcircleo" size={50} color="white" />
+                    </Pressable>
 
-                <View className='flex-1 justify-center' >
-                    <View className='mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center'>
-                        <Text className='text-4xl text-blue-800' >00:00</Text>
+                    <View className='flex-1 justify-center' >
+                        <View className='mx-auto bg-neutral-200 rounded-full w-44 h-44 justify-center items-center'>
+                            <Text className='text-4xl text-blue-800 font-rmono' >
+                                {formattedTimeMinutes}:{formattedTimeSeconds}
+                            </Text>
+                        </View>
                     </View>
-                </View>
 
-                <View className='mb-5'>
-                    <CustomButton
-                        title='Start Meditation'
-                        onPress={()=> console.log("meditation start!")}
-                    >
+                    <View className='mb-5'>
+                        <CustomButton
+                            title='Start Meditation'
+                            onPress={()=> setMeditating(true)}
+                        >
 
-                    </CustomButton>
-                </View>
-            </AppGradient>
-        </ImageBackground>
-    </View> 
+                        </CustomButton>
+                    </View>
+                </AppGradient>
+            </ImageBackground>
+        </View> 
   )
 }
 
